006c11a1a4c8d9a56eeee065aca3ece4c8806141
hackCat1236 <88300979+hackCat1236@users.noreply.github.com>
2023-06-08 16:41:00 +0530

    my Netbean Project
    my projecte

---
+++ Bha12/src/bha12/Bha12.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bha12;
+
+/**
+ *
+ * @author Lab User
+ */
+public class Bha12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
